3d3dc3f9815bf9be470c997a0ea23d33767e5c46
jeezs <contact@atome.io>
2020-04-14 21:41:23 +0200

    first rspec test

--- atome/spec/atome_spec.rb
+++ atome/spec/atome_spec.rb
@@ -9,3 +9,12 @@ describe Atome do
 end
 
 
+describe Atome do
+  context 'box' do
+    xit { is_expected.to be_an(Atome)  }
+    #xit { is_expected.to be_an(Atome) woith default paramaters such as width, height color x,y }
+  end
+end
+
+
+
