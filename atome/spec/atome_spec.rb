ce0e3fc0cd4456b300c0fb22f05a79eee426a7b6
Romain Tartière <romain@blogreen.org>
2020-04-14 10:00:21 -1000

    Factorize duplicate code

--- atome/spec/atome_spec.rb
+++ atome/spec/atome_spec.rb
@@ -7,6 +7,14 @@ class Dummyrenderer
 end
 
 describe Atome do
+  shared_examples 'ensure atome color is red' do
+    it 'should be red' do
+      expect(subject.color.atome.first).to eq(:red)
+      #      ^^^^^^^^^^^^^^^^^^^^^^^^^
+      #      FIXME: Replace this with something that makes sense
+    end
+  end
+
   subject do
     described_class.new(params.merge(renderer: :dummyrenderer))
   end
@@ -21,11 +29,7 @@ describe Atome do
     context 'with a color parameter' do
       let(:params) { { color: :red } }
 
-      it 'should be red' do
-        expect(subject.color.atome.first).to eq(:red)
-        #      ^^^^^^^^^^^^^^^^^^^^^^^^^
-        # FIXME: Replace this with something that makes sense
-      end
+      include_examples 'ensure atome color is red'
     end
   end
 
@@ -34,9 +38,7 @@ describe Atome do
       expect(subject.color(:red)).to eq(subject)
     end
 
-    it 'should be red' do
-      expect(subject.color.atome.first).to eq(:red)
-    end
+    include_examples 'ensure atome color is red'
   end
 
   context '#color=' do
@@ -44,8 +46,6 @@ describe Atome do
       subject.color = :red
     end
 
-    it 'should be red' do
-      expect(subject.color.atome.first).to eq(:red)
-    end
+    include_examples 'ensure atome color is red'
   end
 end
