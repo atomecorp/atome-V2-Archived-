7726817a7f9fac50dd900c8bc470db06097287c0
Régis Ramillien <r.ramillien@tritonit.tech>
2021-02-17 20:39:24 +0100

    Drawing helper now draw a squiggle.

--- www/public/js/atome_libraries/drawing_helper.js
+++ www/public/js/atome_libraries/drawing_helper.js
@@ -33,38 +33,117 @@ class DrawingHelper {
         this.frame.on("ready", () => {
             const stage = this.frame.stage;
 
-            // const circle = new zim.Circle(50, green).loc(200, 200);
-            // stage.update();
+            var backing = new Rectangle(this.frame.width, this.frame.height, white).center();
 
-            var points = [];
-            const squiggle = new Squiggle({
-                color: blue,
-                thickness: 4
-            });
-            var init = false;
+            // const line = new Shape().addTo();
+
+            const precision = 50;
+            const curving = 0.25;
+            const color = blue;
+            const thickness = 4;
+
+            let points = [];
+            let squiggle;
+            let squiggleInitialised = false;
 
             function beginDraw(x, y) {
-                // circle
-                //     .top()
-                //     .animate({x, y}, 0.5);
+                points.push([x,y]);
 
-                points.push([x, y, 0, 0, -30, 0, 30, 0, "mirror"]);
-                squiggle.points = points;
-                if(!init) {
-                    squiggle.addTo();
-                    // var tm = new TransformManager([squiggle]);
-                    init = true;
-                }
+                squiggle = new Squiggle({
+                    color: color,
+                    thickness: thickness,
+                    controlType: "none",
+                    interactive: false
+                });
 
                 stage.update();
             }
 
-            stage.on("stagemousedown", function(e) {
+            function mouseDrag(x, y) {
+                const lastX = points[points.length - 1][0];
+                const lastY = points[points.length - 1][1];
+
+                if (dist(lastX, lastY, x, y) > precision) {
+                    points.push([x, y]);
+                    squiggle.points = points;
+
+                    if(!squiggleInitialised) {
+                        squiggle.addTo();
+                        squiggleInitialised = true;
+                    }
+                }
+                stage.update();
+            }
+
+            function mouseUp(x, y) {
+                points.push([x, y]);
+
+                zim.loop(squiggle.pointObjects, function(obj, i, t){
+                    if (i===0) return;
+                    if (i===t-1) return;
+                    const previousPoint = squiggle.pointControls[i - 1];
+                    const nextPoint = squiggle.pointControls[i + 1];
+                    const middleX = (previousPoint.x + nextPoint.x) / 2;
+                    const middleY = (previousPoint.y + nextPoint.y) / 2;
+
+                    const w = middleX - obj[0].x;
+                    const h = middleY - obj[0].y;
+
+                    let a = Math.atan2(h, w) * zim.DEG;
+                    a = (360+a)%360;
+                    const a2 = zim.angle(obj[0].x, obj[0].y, nextPoint.x, nextPoint.y);
+
+                    if (a > a2) {
+                        a = (a-a2 < 180) ? a - 90 : a + 90;
+                    } else {
+                        a = (a2-a < 180) ? a + 90 : a - 90;
+                    }
+
+                    const d = zim.dist(previousPoint.x, previousPoint.y, nextPoint.x, nextPoint.y) / 2 * curving;
+
+                    obj[2].x=-d;
+                    obj[2].y=0;
+                    obj[3].x=d;
+                    obj[3].y=0;
+                    obj[3].color = red;
+                    obj[0].rotation=a;
+
+                    obj[4] = "mirror";
+                });
+                squiggle.update();
+
+                points = [];
+                squiggleInitialised = false;
+
+                stage.update();
+            }
+
+            backing.on("mousedown", function(e) {
                 switch (self.mode) {
                     case self.modeType.Use:
                         break;
                     case self.modeType.Draw:
                         beginDraw(self.frame.mouseX, self.frame.mouseY);
+                        break;
+                }
+            });
+
+            backing.on("pressmove", function(e) {
+                switch (self.mode) {
+                    case self.modeType.Use:
+                        break;
+                    case self.modeType.Draw:
+                        mouseDrag(self.frame.mouseX, self.frame.mouseY);
+                        break;
+                }
+            });
+
+            backing.on("pressup", function () {
+                switch (self.mode) {
+                    case self.modeType.Use:
+                        break;
+                    case self.modeType.Draw:
+                        mouseUp(self.frame.mouseX, self.frame.mouseY);
                         break;
                 }
             });
